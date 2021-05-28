4ac2943bcb9438c0e849e0d2e2fb7c46e3b187f5
muammarahlnn <21muammarahlan@gmail.com>
2021-05-29 04:15:11 +0800

    ADD: RecyclerView AdminPembersih

---
+++ app/src/main/java/com/example/npe_02_beclean/Adapters/AdminPembersihAdapter.java
@@ -0,0 +1,95 @@
+package com.example.npe_02_beclean.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.npe_02_beclean.Helpers.Util;
+import com.example.npe_02_beclean.Models.AdminPembersih;
+import com.example.npe_02_beclean.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class AdminPembersihAdapter extends RecyclerView.Adapter<AdminPembersihAdapter.ViewHolder> {
+    private OnItemClick onItemClick;
+    private List<AdminPembersih> adminPembersihList;
+
+    public AdminPembersihAdapter(List<AdminPembersih> adminPembersihList, OnItemClick onItemClick) {
+        this.onItemClick = onItemClick;
+        this.adminPembersihList = adminPembersihList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_pembersih, parent, false);
+
+        return new AdminPembersihAdapter.ViewHolder(view, onItemClick);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // set data to widgets on recyclerview item
+        Picasso.with(holder.itemView.getContext())
+                .load(adminPembersihList.get(position).getImageUrl())
+                .fit()
+                .into(holder.ivImage);
+        holder.tvName.setText(adminPembersihList.get(position).getName());
+        holder.tvTotalMember.setText(String.valueOf(adminPembersihList.get(position).getTotalMember()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return adminPembersihList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        OnItemClick onItemClick;
+        ImageView ivImage;
+        TextView tvName, tvTotalMember;
+        ImageButton btnEdit, btnDelete;
+
+        public ViewHolder(@NonNull View itemView, OnItemClick onItemClick) {
+            super(itemView);
+            this.onItemClick = onItemClick;
+
+            // initialize widgets
+            ivImage = itemView.findViewById(R.id.iv_image_item_pembersih);
+            tvName = itemView.findViewById(R.id.tv_team_name_item_pembersih);
+            tvTotalMember = itemView.findViewById(R.id.tv_total_member_item_pembersih);
+            btnEdit = itemView.findViewById(R.id.ib_edit_item_pembersih);
+            btnDelete = itemView.findViewById(R.id.ib_delete_item_pembersih);
+
+            // if button clicked
+            btnEdit.setOnClickListener(this);
+            btnDelete.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.ib_edit_item_pembersih:
+                    onItemClick.btnEditClicked(getAbsoluteAdapterPosition());
+                    break;
+                case R.id.ib_delete_item_pembersih:
+                    onItemClick.btnDeleteClicked(getAbsoluteAdapterPosition());
+                    break;
+            }
+        }
+    }
+
+    public interface OnItemClick {
+        void btnEditClicked(int position);
+        void btnDeleteClicked(int position);
+    }
+}
--- app/src/main/java/com/example/npe_02_beclean/Admin/PembersihActivity.java
+++ app/src/main/java/com/example/npe_02_beclean/Admin/PembersihActivity.java
@@ -1,24 +1,95 @@
 package com.example.npe_02_beclean.Admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.npe_02_beclean.Activities.TimPembersihActivity;
+import com.example.npe_02_beclean.Adapters.AdminPembersihAdapter;
+import com.example.npe_02_beclean.Models.AdminPembersih;
+import com.example.npe_02_beclean.Models.TimPembersih;
 import com.example.npe_02_beclean.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class PembersihActivity extends AppCompatActivity  {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PembersihActivity extends AppCompatActivity implements AdminPembersihAdapter.OnItemClick {
+
+    // recyclerview attr
+    private RecyclerView rvPembersihAdmin;
+    private AdminPembersihAdapter adminPembersihAdapter;
+    private List<AdminPembersih> adminPembersihList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pembersih);
 
+        // initialize adapter with empty list
+        adminPembersihList = new ArrayList<>();
+        adminPembersihAdapter = new AdminPembersihAdapter(adminPembersihList, this);
 
+        // set recyclerview
+        rvPembersihAdmin = findViewById(R.id.rv_pembersih_admin);
+        rvPembersihAdmin.setHasFixedSize(true);
+        rvPembersihAdmin.setLayoutManager(new LinearLayoutManager(this));
+        rvPembersihAdmin.setAdapter(adminPembersihAdapter);
+
+        // update adapter list with data from firebase
+        updatePembersihList();
+
+    }
+
+    private void updatePembersihList() {
+        // get tim reference
+        DatabaseReference pembersihanRef = FirebaseDatabase.getInstance().getReference()
+                .child("teams");
+        pembersihanRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    // update the adapter's list
+                    AdminPembersih adminPembersih = new AdminPembersih();
+                    adminPembersih.setName(data.child("name").getValue().toString());
+                    adminPembersih.setImageUrl(data.child("url_img").getValue().toString());
+                    adminPembersih.setCost(Integer.parseInt(data.child("cost").getValue().toString()));
+                    adminPembersih.setTotalMember(Integer.parseInt(data.child("total_member").getValue().toString()));
+                    adminPembersih.setRating(Integer.parseInt(data.child("rating").getValue().toString()));
+                    adminPembersihList.add(adminPembersih);
+                }
+                adminPembersihAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(PembersihActivity.this, "Terjadi kesalahan pada database.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void got(View view) {
         Intent i = new Intent(this, AddPembersihActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public void btnEditClicked(int position) {
+        Toast.makeText(this, adminPembersihList.get(position).getName() + " clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void btnDeleteClicked(int position) {
+        Toast.makeText(this, adminPembersihList.get(position).getName() + " clicked", Toast.LENGTH_SHORT).show();
     }
 }
